13730a41f65ac6d020f2bd1da68dddd3877173ea
Rajat Thareja <rajat.thareja.1990@gmail.com>
2018-01-03 17:37:40 +0530

    WebSocket

---
+++ src/main/java/game/cards/configuration/WebSocketConfiguration.java
@@ -0,0 +1,29 @@
+package game.cards.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfiguration extends AbstractWebSocketMessageBrokerConfigurer {
+
+    @Value("${allowed.origins}")
+    private String allowedOrigins;
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/socket")
+                .setAllowedOrigins(allowedOrigins.split(","))
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app")
+                .enableSimpleBroker("/players", "/started", "/activePlayerId");
+    }
+}
---
+++ src/main/java/game/cards/controller/WebSocketController.java
@@ -0,0 +1,35 @@
+package game.cards.controller;
+
+import game.cards.bluff.Game;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+import java.util.HashMap;
+
+@Controller
+public class WebSocketController {
+
+    private final SimpMessagingTemplate template;
+
+    @Autowired
+    WebSocketController(SimpMessagingTemplate template) {
+        this.template = template;
+    }
+
+    @MessageMapping("/update")
+    public void update(String message) {
+        switch (message) {
+            case "players":
+                this.template.convertAndSend("/players", Game.getPlayers());
+                break;
+            case "started":
+                this.template.convertAndSend("/started", Game.isGameStarted());
+                break;
+            case "activePlayerId":
+                this.template.convertAndSend("/activePlayerId", Game.getActivePlayerId());
+                break;
+        }
+    }
+}
